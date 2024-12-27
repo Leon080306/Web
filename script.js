67165f742ca0b6e0ea8a9f313decbe5e0a48b0ce
Leon080306 <leonsamuels080306@gmail.com>
2024-12-27 22:07:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -102,91 +102,92 @@ $(document).ready(function () {
                 $("#a14").hide();
             })
         }
+
+
         else {
             $("#a1").show();
             $("#a1").animate({
                 top: "20%", 
                 left: "45%"
-            }, 750);
-
-            $("#a2").show();
-            $("#a2").animate({
-                top: "10%", 
-                left: "60%"
-            }, 750);
-
-            $("#a3").show();
-            $("#a3").animate({
-                top: "0%", 
-                left: "75%"
-            }, 750);
-
-            $("#a4").show();
-            $("#a4").animate({
-                top: "10%", 
-                left: "90%"
-            }, 750);
-
-            $("#a5").show();
-            $("#a5").animate({
-                top: "25%", 
-                left: "80%"
-            }, 750);
-
-            $("#a6").show();
-            $("#a6").animate({
-                top: "40%", 
-                left: "70%"
-            }, 750);
-
-            $("#a7").show();
-            $("#a7").animate({
-                top: "55%", 
-                left: "60%"
-            }, 750);
-
-            $("#a8").show();
-            $("#a8").animate({
-                top: "70%", 
-                left: "45%"
-            }, 750);
-
-            $("#a9").show();
-            $("#a9").animate({
-                top: "55%", 
-                left: "30%"
-            }, 750);
-
-            $("#a10").show();
-            $("#a10").animate({
-                top: "40%", 
-                left: "20%"
-            }, 750);
-
-            $("#a11").show();
-            $("#a11").animate({
-                top: "25%", 
-                left: "10%"
-            }, 750);
-
-            $("#a12").show();
-            $("#a12").animate({
-                top: "10%", 
-                left: "0%"
-            }, 750);
-
-            $("#a13").show();
-            $("#a13").animate({
-                top: "0%", 
-                left: "15%"
-            }, 750);
-
-            $("#a14").show();
-            $("#a14").animate({
-                top: "10%", 
-                left: "30%"
-            }, 750);
-        }
-        
+            }, 250, function () {
+                $("#a2").show();
+                $("#a2").animate({
+                    top: "10%", 
+                    left: "60%"
+                }, 250, function () {
+                    $("#a3").show();
+                    $("#a3").animate({
+                        top: "0%", 
+                        left: "75%"
+                    }, 250, function () {
+                        $("#a4").show();
+                        $("#a4").animate({
+                            top: "10%", 
+                            left: "90%"
+                        }, 250, function () {
+                            $("#a5").show();
+                            $("#a5").animate({
+                                top: "25%", 
+                                left: "80%"
+                            }, 250, function () {
+                                $("#a6").show();
+                                $("#a6").animate({
+                                    top: "40%", 
+                                    left: "70%"
+                                }, 250, function () {
+                                    $("#a7").show();
+                                    $("#a7").animate({
+                                        top: "55%", 
+                                        left: "60%"
+                                    }, 250, function () {
+                                        $("#a8").show();
+                                        $("#a8").animate({
+                                            top: "70%", 
+                                            left: "45%"
+                                        }, 250, function () {
+                                            $("#a9").show();
+                                            $("#a9").animate({
+                                                top: "55%", 
+                                                left: "30%"
+                                            }, 250, function () {
+                                                $("#a10").show();
+                                                $("#a10").animate({
+                                                    top: "40%", 
+                                                    left: "20%"
+                                                }, 250, function () {
+                                                    $("#a11").show();
+                                                    $("#a11").animate({
+                                                        top: "25%", 
+                                                        left: "10%"
+                                                    }, 250, function () {
+                                                        $("#a12").show();
+                                                        $("#a12").animate({
+                                                            top: "10%", 
+                                                            left: "0%"
+                                                        }, 250, function () {
+                                                            $("#a13").show();
+                                                            $("#a13").animate({
+                                                                top: "0%", 
+                                                                left: "15%"
+                                                            }, 250, function () {
+                                                                $("#a14").show();
+                                                                $("#a14").animate({
+                                                                    top: "10%", 
+                                                                    left: "30%"
+                                                                }, 250);
+                                                            });
+                                                        });
+                                                    });
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        }       
     })
 })
